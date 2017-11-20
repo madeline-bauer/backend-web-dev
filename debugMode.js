cb1a363067af276985a626c723220b7971f5716d
TCJ Design Github <tcj@tcj.design>
2017-11-20 15:59:17 -0500

    Debug Mode
    Debug mode created

--- debugMode.js
+++ debugMode.js
@@ -1,2 +1,6 @@
 var debug = true;
-exports.debug = debug;
+var verbose = true;
+var allowInserts = true;
+exports.debug = debug; // export whether or not debug mode should be active as a boolean
+exports.allowInserts = allowInserts; // export whether or not inserting into the db is allowed as a boolean
+exports.verbose = verbose; // export whether or not to log verbosely
